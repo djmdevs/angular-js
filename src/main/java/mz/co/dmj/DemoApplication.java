594728507b0e41a0ebba1800f9d66ae4b4c305e1
djmdevs <djmdevs@gmail.com>
2021-04-04 20:02:13 +0200

    uc_003-Updating build on gradle

--- src/main/java/mz/co/dmj/DemoApplication.java
+++ src/main/java/mz/co/dmj/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package mz.co.dmj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/mz/co/dmj/DemoApplicationTests.java
+++ src/test/java/mz/co/dmj/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package mz.co.dmj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
