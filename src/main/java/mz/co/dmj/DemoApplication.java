4cba49893114b5c10361d0b9f57b21ee1d58fed5
djmdevs <djmdevs@gmail.com>
2021-04-04 21:06:25 +0200

    uc_005-Creating Rest configuration

--- src/main/java/mz/co/dmj/DemoApplication.java
+++ src/main/java/mz/co/dmj/DemoApplication.java
@@ -2,12 +2,21 @@ package mz.co.dmj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/Hi")
+	public String greeting() {
+		
+		return "Hellow DMj";
 	}
 
 }
